2f29a31d6f9beed1ae4ba1835bffc1038da6d037
l3n3mYl <asavkinas123@gmail.com>
2022-02-01 10:12:37 +0000

    as

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use(express.json({ extended: false }))
 
 app.use('/api/product', product)
 
+app.get('/', (req, res) => {
+  res.end('DONE')
+})
+
 const PORT = process.env.PORT || 9000
 
 app.listen(PORT, () => console.log("Server running"))
